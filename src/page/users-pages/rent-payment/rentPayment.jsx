2e8756b61b82b9a6d66603ebe6ec7b33dd163fa8
obadaa-kahlous <obadaakahlous@gmail.com>
2023-10-25 22:29:58 +0300

    add popup for payment page

--- src/page/users-pages/rent-payment/rentPayment.jsx
+++ src/page/users-pages/rent-payment/rentPayment.jsx
@@ -19,15 +19,20 @@ import angularSide from "../../../assets/2019-chevrolet-camaro-1lt-coupe-angular
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 // import { Form, Link } from "react-router-dom";
 import { useState } from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, Switch, TextField } from "@mui/material";
 import { ErrorMessage, Field, Formik, Form } from "formik";
 import * as yup from "yup";
+import right from "../../../assets/right.svg";
+import Redstar from "../../../assets/red-star.svg";
+import wrong from "../../../assets/false.svg";
+
+const label = { inputProps: { "aria-label": "Switch demo" } };
 const RentPayment = () => {
   const validationSchema = yup.object().shape({
     email: yup.string().email().required(),
     cardNumber: yup
       .string()
-      .matches(/^[0-9]{16}$/, "Invalid card number")
+      .matches(/^[0-9]{3}$/, "Invalid card number")
       .required(),
     expiryDate: yup
       .string()
@@ -38,11 +43,29 @@ const RentPayment = () => {
       .matches(/^[0-9]{3}$/, "Invalid CVV")
       .required(),
   });
+  const validationSchemaCheck = yup.object().shape({
+    promoCode: yup
+      .string()
+      .matches(/^[0-9]{6}$/, "Invalid card number")
+      .required(),
+  });
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [isCheckOut, setIsCheckout] = useState();
+  const [isDone, setIsDone] = useState(false);
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
+  };
+  const handleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+  const handleCheckout = () => {
+    setIsCheckout(!isCheckOut);
+  };
+  const handledone = () => {
+    setIsDone(!isDone);
   };
 
   const handleButtonClick = (event) => {
@@ -431,6 +454,10 @@ const RentPayment = () => {
                           lineHeight: "normal",
                         }}
                         disabled={!formik.isValid || !formik.dirty}
+                        onClick={() => {
+                          handleChecked();
+                          togglePopup();
+                        }}
                       >
                         Confirm Card
                       </Button>
@@ -447,6 +474,332 @@ const RentPayment = () => {
               }
               `}
             </style>
+          </div>
+        )}
+
+        {isChecked && (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "10000",
+            }}
+            onClick={handleChecked}
+          >
+            <div
+              className=" popup w-[230px] p-[20px]  flex items-center flex-col justify-center gap-[10px] rounded-[15px] "
+              onClick={(event) => {
+                handleButtonClick(event);
+              }}
+            >
+              <div className="w-[50px] h-[50px] rounded-[50%] flex justify-center items-center border-[2px] border-[#3D3C3C] ">
+                <img src={right} alt="right" className="w-[30px]" />
+              </div>
+              <p className="text-[#3D3C3C] font-[700] text-[14px] mb-[20px]">
+                Card Confirmed
+              </p>
+              <button
+                className="w-[100%] bg-[#E42E35] text-[#F5FAFF] font-[700] py-[10px] text-[14px]"
+                onClick={() => {
+                  handleChecked();
+                  handleCheckout();
+                }}
+              >
+                Continue To Checkout
+              </button>
+            </div>
+          </div>
+        )}
+
+        {isCheckOut && (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "10000",
+            }}
+            onClick={handleCheckout}
+          >
+            <div
+              className=" popup w-[350px] p-[30px]   rounded-[15px] "
+              onClick={(event) => {
+                handleButtonClick(event);
+              }}
+            >
+              <p className="text-[#3D3C3C] font-[700] text-[20px] mt-[15px] mb-[5px] ">
+                Checkout
+              </p>
+              <p className="mb-[20px] text-[#3D3C3C]">Chevrolet Camaro</p>
+              <div className="flex">
+                <p className="text-[#E42E35] font-[700]">2,500 </p>
+                <img src={Redstar} alt="star" className="w-[18px]" />
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="text-[#3D3C3C] font-[700]">Redeem from points</p>
+                <Switch {...label} />
+              </div>
+
+              <div className="text-center grow ">
+                <Formik
+                  validationSchema={validationSchemaCheck}
+                  initialValues={{
+                    promoCode: "",
+                  }}
+                  // validationSchema={validationSchema}
+                  onSubmit={(values, { resetForm }, setSubmitting) => {
+                    console.log({ values });
+                    resetForm();
+                    setSubmitting(false);
+                  }}
+                >
+                  {(formik) => (
+                    <Form className="h-[100%] flex flex-col justify-between ">
+                      <div className="">
+                        <div className="w-[100%]">
+                          <Field
+                            as={TextField}
+                            name="promoCode"
+                            id="promoCode"
+                            label="Promo Code"
+                            variant="outlined"
+                            type="password"
+                            error={
+                              formik.errors.promoCode &&
+                              formik.touched.promoCode
+                            }
+                            InputLabelProps={{
+                              style: {
+                                color: "#353B42",
+                                fontSize: "15px",
+                                fontWeight: "700",
+                                opacity: "0.75",
+                              },
+                            }}
+                            sx={{
+                              borderRadius: "8px",
+                              width: "100%",
+                              marginTop: "25px",
+                              "& .MuiOutlinedInput-root": {
+                                "& fieldset": {
+                                  border: "2px solid rgba(62, 63, 67, 0.60)",
+                                  borderRadius: "12px",
+                                },
+                                "&:hover fieldset": {
+                                  border: "2px solid rgba(62, 63, 67, 0.60)",
+                                  borderRadius: "12px",
+                                },
+                                "&.Mui-focused fieldset": {
+                                  border: "2px solid rgba(62, 63, 67, 0.60)",
+                                  borderRadius: "12px",
+                                },
+                              },
+                            }}
+                          />
+                          <ErrorMessage name="promoCode">
+                            {(msg) => (
+                              <p className=" text-[14px] text-[red]">{msg}</p>
+                            )}
+                          </ErrorMessage>
+                        </div>
+                      </div>
+
+                      <div className="flex justify-between items-center mt-[25px] mb-[8px]">
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          50 days rent
+                        </p>
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          25,000 A.E.D
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center mb-[8px]">
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          insurance deposit
+                        </p>
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          500 A.E.D
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center mb-[8px]">
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          Driver
+                        </p>
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          6,000 A.E.D
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center mb-[8px]">
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          points
+                        </p>
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          - 5,644 A.E.D
+                        </p>
+                      </div>
+                      <div className="w-[100%] bg-[#3D3C3C] h-[2px]"></div>
+                      <div className="flex justify-between items-center  mb-[25px] mt-[15px]">
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          Total
+                        </p>
+                        <p className="text-[#3D3C3C] text-[14px] font-[600]">
+                          31,000 A.E.D
+                        </p>
+                      </div>
+
+                      <Button
+                        variant="contained"
+                        type="submit"
+                        color="error"
+                        sx={{
+                          borderRadius: "12px",
+                          width: "90%",
+                          boxShadow: "0px 4px 31px 0px rgba(0, 0, 0, 0.25)",
+                          background: "var(--P1, #E42E35)",
+                          color: "#F5FAFF",
+                          fontSize: "15px",
+                          fonrWeight: "700",
+                          fontFamily: "Quicksand",
+                          padding: "10px",
+                          alignSelf: "center",
+                          lineHeight: "normal",
+                        }}
+                        disabled={!formik.isValid || !formik.dirty}
+                        onClick={() => {
+                          handledone();
+                          handleCheckout();
+                        }}
+                      >
+                        Checkout
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* {isDone ? (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "10000",
+            }}
+            onClick={handledone}
+          >
+            <div
+              className=" popup w-[230px] p-[20px] h-[200px] flex items-center flex-col justify-center gap-[10px] rounded-[15px] "
+              onClick={(event) => {
+                handleButtonClick(event);
+              }}
+            >
+              <div className="w-[50px] h-[50px] rounded-[50%] flex justify-center items-center border-[2px] border-[#3D3C3C] ">
+                <img src={right} alt="right" className="w-[30px]" />
+              </div>
+              <p className="text-[#3D3C3C] font-[700] text-[14px]">
+                Payment Confirmed
+              </p>
+              <button
+                className="w-[100%] bg-[#E42E35] text-[#F5FAFF] font-[700] py-[10px] text-[14px]"
+                onClick={handledone}
+              >
+                Done!
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "10000",
+            }}
+            onClick={handledone}
+          >
+            <div
+              className=" popup w-[230px] p-[20px] h-[200px] flex items-center flex-col justify-center gap-[10px] rounded-[15px] "
+              onClick={(event) => {
+                handleButtonClick(event);
+              }}
+            >
+              <img src={wrong} alt="wrong" className="w-[50px]" />
+
+              <p className="text-[#3D3C3C] font-[700] text-[14px] mb-[20px]">
+                Payment Unsuccessful
+              </p>
+              <button
+                className="w-[100%] bg-[#E42E35] text-[#F5FAFF] font-[700] py-[10px] text-[14px]"
+                onClick={handledone}
+              >
+                Done!
+              </button>
+            </div>
+          </div>
+        )} */}
+        
+        {isDone && (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: "10000",
+            }}
+            onClick={handledone}
+          >
+            <div
+              className=" popup w-[230px] p-[20px] h-[200px] flex items-center flex-col justify-center gap-[10px] rounded-[15px] "
+              onClick={(event) => {
+                handleButtonClick(event);
+              }}
+            >
+              <div className="w-[50px] h-[50px] rounded-[50%] flex justify-center items-center border-[2px] border-[#3D3C3C] ">
+                <img src={right} alt="right" className="w-[30px]" />
+              </div>
+              <p className="text-[#3D3C3C] font-[700] text-[14px] mb-[20px]">
+                Payment Confirmed
+              </p>
+              <button
+                className="w-[100%] bg-[#E42E35] text-[#F5FAFF] font-[700] py-[10px] text-[14px]"
+                onClick={handledone}
+              >
+                Done!
+              </button>
+            </div>
           </div>
         )}
       </div>
